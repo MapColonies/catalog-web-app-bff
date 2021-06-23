4875afa560bcb4d54f99ad1de3fd99496c313c14
alebinson <newdarkvirus@gmail.com>
2021-06-23 11:22:18 +0300

    feat: entity descriptor holds fields ui aspects and translations

---
+++ src/common/translations/record-raster-fields.translation.ts
@@ -0,0 +1,88 @@
+const pycswLayerCatalogRecordTranslation = {
+  id: {
+    label: 'field-names.raster.id',
+  },
+  productId: {
+    label: 'field-names.raster.productId',
+  },
+  productVersion: {
+    label: 'field-names.raster.productVersion',
+  },
+  productType: {
+    label: 'field-names.raster.productType',
+  },
+  type: {
+    label: 'field-names.raster.type',
+  },
+  resolution: {
+    label: 'field-names.raster.resolution',
+  },
+  updateDate: {
+    label: 'field-names.raster.update-date',
+  },
+  description: {
+    label: 'field-names.raster.description',
+    fullWidth: true,
+  },
+  sensorType: {
+    label: 'field-names.raster.sensor-type',
+    isManuallyEditable: true,
+    fullWidth: true,
+  },
+  region: {
+    label: 'field-names.raster.region',
+    fullWidth: true,
+  },
+  classification: {
+    label: 'field-names.raster.classification',
+    fullWidth: true,
+    // translate: {
+    //   dictionary: {
+    //     "SHAMUR": 'field-values.raster.classification.shamur'
+    //   }
+    // } ,
+  },
+  links: {
+    label: 'field-names.raster.links',
+    fullWidth: true,
+  },
+  'link.name': {
+    label: 'field-names.raster.link.name',
+    fullWidth: true,
+  },
+  'link.description': {
+    label: 'field-names.raster.link.description',
+    fullWidth: true,
+  },
+  'link.protocol': {
+    label: 'field-names.raster.link.protocol',
+    fullWidth: true,
+  },
+  'link.url': {
+    label: 'field-names.raster.link.url',
+    fullWidth: true,
+  },
+  creationDate: {
+    label: 'field-names.raster.creation-date',
+  },
+  ingestionDate: {
+    label: 'field-names.raster.ingestion-date',
+  },
+  sourceDateStart: {
+    label: 'field-names.raster.source-start-date',
+  },
+  sourceDateEnd: {
+    label: 'field-names.raster.source-end-date',
+  },
+  accuracyCE90: {
+    label: 'field-names.raster.accuracyCE90',
+  },
+  srsId: {
+    label: 'field-names.raster.srs',
+  },
+  srsName: {
+    label: 'field-names.raster.srs-name',
+  },
+} as Record<string, any>;
+
+export const pycswLayerCatalogRecordUIAspects = pycswLayerCatalogRecordTranslation;
