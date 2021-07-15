5599081ca04e7914bf1f7f813ac566c1005438ba
Ella <ellamartirosyan@gmail.com>
2021-07-15 17:34:44 +0300

    feat: ui aspects
    ui aspects

---
+++ src/common/ui-aspects/enum.translation.ts
@@ -0,0 +1,14 @@
+import { enumKeys } from '../../helpers/enums';
+
+export function updateDictionary(propName: string, type: any): Record<string, unknown> {
+  const dictionary: Record<string, unknown> = {};
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  for (const value of enumKeys<object>(type)) {
+    dictionary[value] = {
+      displayKey: `lookups.${propName}.${(value as string).toLowerCase()}`,
+      tooltipKey: `lookups.${propName}.${(value as string).toLowerCase()}.tooltip`,
+      icon: `mc-icon-${(value as string).toLowerCase()}`,
+    };
+  }
+  return dictionary;
+}
