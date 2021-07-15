cb49702f833a9b86475c60c5ccd9ae717c308702
Ella <ellamartirosyan@gmail.com>
2021-07-15 16:15:25 +0300

    fix: revert jobs & tasks
    Revert jobs & tasks

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,9 +1,8 @@
 import { NonEmptyArray } from 'type-graphql';
 import { LayerMetadataMixedResolver } from './resolvers/csw.resolver';
 import { EntityDescriptorResolver } from './resolvers/entity-descriptor.resolver';
-import { JobResolver } from './resolvers/job.resolver';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 export function getResolvers(): NonEmptyArray<Function> | NonEmptyArray<string> {
-  return [LayerMetadataMixedResolver, EntityDescriptorResolver, JobResolver];
+  return [LayerMetadataMixedResolver, EntityDescriptorResolver];
 }
