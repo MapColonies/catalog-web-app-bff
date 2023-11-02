0b894b5a36b59ee9af84f354c09bf78b832cf33c
ellamartirosyan <ellamartirosyan@gmail.com>
2023-11-02 11:17:41 +0200

    chore: not in use

--- src/graphql/resolvers/dem-heights.resolver.ts
+++ src/graphql/resolvers/dem-heights.resolver.ts
@@ -1,8 +1,8 @@
 import { container } from 'tsyringe';
-import { Arg, Ctx, FieldResolver, Query, Resolver, Root } from 'type-graphql';
+import { Arg, Ctx, Query, Resolver } from 'type-graphql';
 import { IContext } from '../../common/interfaces';
-import { GetDemPointsHeightsInput } from '../inputTypes';
 import DemHeightsManager from '../../common/dem-heights-manager/dem-heights-manager';
+import { GetDemPointsHeightsInput } from '../inputTypes';
 import { PositionWithHeight, PositionsWithHeights, Product } from '../dem-heights';
 
 @Resolver((of) => PositionWithHeight)
@@ -24,18 +24,4 @@ export class DemHeightsResolver {
 
     return positionsWithHeights;
   }
-
-  // There is an issue serializing dates that comes in string format with TypeGraphQL, this is a workaround
-  // @FieldResolver()
-  // public updateDate(@Root() product: Product): Date | null {
-  //   if (product.updateDate instanceof Date) {
-  //     return product.updateDate;
-  //   }
-
-  //   if (typeof product.updateDate === 'string') {
-  //     return new Date(product.updateDate);
-  //   }
-
-  //   return null;
-  // }
 }
