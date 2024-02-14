4b5c47661a655acffe9fb012556aa655c00605ff
alebinson <newdarkvirus@gmail.com>
2024-02-14 12:48:06 +0200

    chore: lint

--- src/graphql/user-login-verifier.ts
+++ src/graphql/user-login-verifier.ts
@@ -11,14 +11,14 @@ export interface IUserLogin {
 
 @singleton()
 export class UserLoginVerifier {
-  private readonly admin_pwd: string;
+  private readonly adminPwd: string;
 
   public constructor(@inject(Services.CONFIG) private readonly config: IConfig, @inject(Services.LOGGER) private readonly logger: Logger) {
-    this.admin_pwd = this.config.get('adminPassword');
+    this.adminPwd = this.config.get('adminPassword');
   }
 
   public async verifyAdminUser(userData: UserLoginParams, ctx?: IContext): Promise<IUserLogin> {
-    const isAdminVerified = this.admin_pwd === userData.userPassword;
+    const isAdminVerified = this.adminPwd === userData.userPassword;
     return new Promise((resolve, reject) => {
       resolve({ isValid: isAdminVerified });
     });
