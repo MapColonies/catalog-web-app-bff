87e1ee101f42b61c521ab78a56c986321cbcbad7
ellamartirosyan <ellamartirosyan@gmail.com>
2024-07-28 21:50:48 +0300

    chore: api

--- src/graphql/source-validator.ts
+++ src/graphql/source-validator.ts
@@ -1,10 +1,12 @@
 import { Logger } from '@map-colonies/js-logger';
+import { AxiosRequestConfig } from 'axios';
 import { IConfig } from 'config';
 import { inject, singleton } from 'tsyringe';
 import { SourceValidationParams } from './inputTypes';
 import { SourceValidation } from './sourceValidation';
 import { Services } from '../common/constants';
 import { IContext, IService } from '../common/interfaces';
+import { requestExecutor } from '../utils';
 
 @singleton()
 export class SourceValidator {
@@ -15,29 +17,28 @@ export class SourceValidator {
   }
 
   public async validateSources(sourceData: SourceValidationParams, ctx: IContext): Promise<SourceValidation> {
-    // this.logger.info(`[SourceValidator][validateSources] start validation for source ${sourceData.originDirectory}.`);
+    this.logger.info(`[SourceValidator][validateSources] start validation for source ${sourceData.originDirectory}.`);
 
-    // const response = await requestExecutor(
-    //     {
-    //       url: `${this.service.url}/ingestion/validateSources`,
-    //       exposureType: this.service.exposureType,
-    //     },
-    //     'POST',
-    //     this.buildPayload(sourceData),
-    //     ctx
-    //   );
+    const response = await requestExecutor(
+      {
+        url: `${this.service.url}/ingestion/validateSources`,
+        exposureType: this.service.exposureType,
+      },
+      'POST',
+      this.buildPayload(sourceData),
+      ctx
+    );
+    return response.data as SourceValidation;
 
-    //   return response.data as SourceValidation;
-
-    return new Promise((resolve, reject) => {
-      resolve({
-        isValid: true,
-        message: 'Files are valid',
-      });
-    });
+    // return new Promise((resolve, reject) => {
+    //   resolve({
+    //     isValid: false,
+    //     message: 'Invalid files',
+    //   });
+    // });
   }
 
-  //   private buildPayload(data: any): import('axios').AxiosRequestConfig {
-  //     throw new Error('Method not implemented.');
-  //   }
+  private buildPayload(data: SourceValidationParams): AxiosRequestConfig {
+    return { data };
+  }
 }
