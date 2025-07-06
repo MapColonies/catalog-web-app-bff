1cdb3a6338cef070183294e304deeb8d6572276e
ellamartirosyan <ellamartirosyan@gmail.com>
2025-07-06 13:53:28 +0300

    chore: pp

--- src/common/ui-aspects/record-polygon-part-fields.ui-aspects.ts
+++ src/common/ui-aspects/record-polygon-part-fields.ui-aspects.ts
@@ -3,26 +3,26 @@ import { DateGranularity, ValidationValueType } from '../../graphql/entityDescri
 const polygonPartRecordAspects = {
   sourceId: {
     label: 'field-names.raster.id',
-    order: 0,
+    order: 2,
     isBriefField: {
       order: 2,
     },
   },
   version: {
     label: 'field-names.raster.productVersion',
-    order: 0,
+    order: 3,
     isBriefField: {
       order: 4,
     },
   },
   sourceName: {
     label: 'field-names.raster.productName',
-    order: 0,
+    order: 1,
     isInfoTooltip: true,
   },
   resolutionDegree: {
     label: 'field-names.raster.maxResolutionDeg',
-    order: 0,
+    order: 999,
     lookupTable: 'zoomlevelresolutions',
     lookupTableBinding: {
       valueFromPropertyName: 'resolutionDeg',
@@ -42,7 +42,7 @@ const polygonPartRecordAspects = {
   },
   ingestionDateUTC: {
     label: 'field-names.raster.ingestion-date',
-    order: 0,
+    order: 5,
     dateGranularity: DateGranularity.DATE_AND_TIME,
     isBriefField: {
       order: 3,
@@ -51,7 +51,7 @@ const polygonPartRecordAspects = {
   },
   description: {
     label: 'field-names.raster.description',
-    order: 0,
+    order: 100,
     fullWidth: true,
     rows: 4,
     isBriefField: {
@@ -60,22 +60,22 @@ const polygonPartRecordAspects = {
   },
   sensors: {
     label: 'field-names.raster.sensors',
-    order: 0,
+    order: 104,
     fullWidth: true,
   },
   countries: {
     label: 'field-names.polygon-parts.countries',
-    order: 0,
+    order: 105,
     fullWidth: true,
   },
   cities: {
     label: 'field-names.polygon-parts.cities',
-    order: 0,
+    order: 106,
     fullWidth: true,
   },
   classification: {
     label: 'field-names.raster.classification',
-    order: 0,
+    order: 101,
     fullWidth: true,
     isBriefField: {
       order: 11,
@@ -83,7 +83,7 @@ const polygonPartRecordAspects = {
   },
   imagingTimeBeginUTC: {
     label: 'field-names.raster.imagingTimeBeginUTC',
-    order: 0,
+    order: 102,
     dateGranularity: DateGranularity.DATE_AND_TIME,
     isBriefField: {
       order: 5,
@@ -91,7 +91,7 @@ const polygonPartRecordAspects = {
   },
   imagingTimeEndUTC: {
     label: 'field-names.raster.imagingTimeEndUTC',
-    order: 0,
+    order: 103,
     dateGranularity: DateGranularity.DATE_AND_TIME,
     isFilterable: {
       participateInFilterPanel: true,
@@ -105,11 +105,11 @@ const polygonPartRecordAspects = {
   },
   horizontalAccuracyCE90: {
     label: 'field-names.raster.minHorizontalAccuracyCE90',
-    order: 0,
+    order: 200,
   },
   resolutionMeter: {
     label: 'field-names.raster.maxResolutionMeter',
-    order: 0,
+    order: 999,
     isBriefField: {
       order: 7,
     },
@@ -118,7 +118,7 @@ const polygonPartRecordAspects = {
   },
   sourceResolutionMeter: {
     label: 'field-names.polygon-parts.sourceResolutionMeter',
-    order: 0,
+    order: 4,
     isBriefField: {
       order: 8,
     },
@@ -126,14 +126,14 @@ const polygonPartRecordAspects = {
   },
   footprint: {
     label: 'field-names.raster.footprint',
-    order: 0,
+    order: 201,
     fullWidth: true,
     isCopyable: true,
   },
   recordId: {
     // *** PROBABLY NO NEED
     label: 'field-names.raster.id',
-    order: 0,
+    order: 999,
   },
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 } as Record<string, any>;
