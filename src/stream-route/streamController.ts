28e0376ee6f3c1644c088df1f53f6d6a0d4e68ed
Eliav Maimon <liavma3@gmail.com>
2025-08-13 12:06:14 +0300

    fix: error handle

--- src/stream-route/streamController.ts
+++ src/stream-route/streamController.ts
@@ -6,21 +6,31 @@ import { StatusCodes } from 'http-status-codes';
 import { RecordType } from '@map-colonies/types';
 import { get } from 'lodash';
 import { HttpError } from '@map-colonies/error-types';
+import { Logger } from '@map-colonies/js-logger';
 import { StorageExplorerManager } from '../common/storage-explorer-manager/storage-explorer-manager';
+import { Services } from '../common/constants';
 
 export type GetStreamer = Stream;
 
 @injectable()
 export class StreamController {
   private readonly storageExplorerManager: StorageExplorerManager;
+  private readonly logger: Logger;
 
   public constructor() {
     this.storageExplorerManager = container.resolve(StorageExplorerManager);
+    this.logger = container.resolve(Services.LOGGER);
   }
 
   public getStreamFile = async (req: Request, res: Response): Promise<void> => {
     try {
       const { path, type } = req.query;
+
+      if (path === undefined || type === undefined) {
+        res.status(StatusCodes.BAD_REQUEST).send({ error: 'Missing query parameters' });
+        return;
+      }
+
       const ctx = { requestHeaders: req.headers };
 
       const stream = await this.storageExplorerManager.getStreamFile(
@@ -46,6 +56,11 @@ export class StreamController {
       const { path, type } = req.query;
       const ctx = { requestHeaders: req.headers };
 
+      if (path === undefined || type === undefined) {
+        res.status(StatusCodes.BAD_REQUEST).send({ error: 'Missing query parameters' });
+        return;
+      }
+
       const response = await this.storageExplorerManager.writeStreamFile(
         {
           path: path as string,
@@ -62,15 +77,16 @@ export class StreamController {
   };
 
   private handleError(err: unknown, res: Response): void {
+    this.logger.error(err);
     if (axios.isAxiosError(err)) {
       const data = err.response?.data as { error?: { message?: string } } | undefined;
       const axiosMessage: string = data?.error?.message ?? err.message;
-      res.status(err.response?.status ?? StatusCodes.INTERNAL_SERVER_ERROR).send(axiosMessage);
+      res.status(err.response?.status ?? StatusCodes.INTERNAL_SERVER_ERROR).send({ error: axiosMessage });
     } else if (err instanceof HttpError) {
-      throw err;
+      res.status(err.status).send({ error: err.message });
     } else {
-      const message = get(err as Record<string, unknown>, 'message');
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(message ?? 'Internal server error');
+      const message = get(err as Record<string, unknown>, 'message', 'Internal server error');
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({ error: message });
     }
   }
 }
