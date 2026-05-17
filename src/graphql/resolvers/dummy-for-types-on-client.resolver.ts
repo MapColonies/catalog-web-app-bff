7abe4f94275cd31b0a5a0da6f32b4cf4a9687aad
ellamartirosyan <ellamartirosyan@gmail.com>
2026-05-17 12:31:26 +0300

    fix: eslint

--- src/graphql/resolvers/dummy-for-types-on-client.resolver.ts
+++ src/graphql/resolvers/dummy-for-types-on-client.resolver.ts
@@ -1,12 +1,20 @@
+import { container } from 'tsyringe';
 import { Query, Resolver } from 'type-graphql';
+import { Logger } from '@map-colonies/js-logger';
+import { Services } from '../../common/constants';
 import { DummyForTypesOnClient } from '../dummy-for-types-on-client';
 
 @Resolver()
 export class DummyForTypesOnClientResolver {
-  public constructor() {}
+  private readonly logger: Logger;
+
+  public constructor() {
+    this.logger = container.resolve(Services.LOGGER);
+  }
 
   @Query(() => DummyForTypesOnClient, { nullable: true })
   public async dummyForTypesOnClient(): Promise<DummyForTypesOnClient> {
-    return {} as DummyForTypesOnClient;
+    this.logger.info('Fetching dummy data for types on client');
+    return await Promise.resolve({} as DummyForTypesOnClient);
   }
 }
