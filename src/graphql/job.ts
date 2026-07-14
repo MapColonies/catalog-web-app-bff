cdfc0420e481d0b934177637d40989524a8ea8d9
ellamartirosyan <ellamartirosyan@gmail.com>
2026-07-14 13:10:39 +0300

    fix: test

--- src/graphql/job.ts
+++ src/graphql/job.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { GraphQLScalarType } from 'graphql';
 import { ObjectType, Field, registerEnumType } from 'type-graphql';
 import { ProductType } from '@map-colonies/types';
